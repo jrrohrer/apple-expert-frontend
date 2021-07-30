c3d25da3b73b65ed19c868a94815b5475ae6ccc5
jrrohrer <jessie.rohrer@gmail.com>
2021-07-30 15:24:47 -0400

    add submit button and GET to retrieve apples

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ function getCategories() {
 function populateCategoryDropdown() {
   getCategories()
   .then(categories => {
-    const dropDown = document.getElementById('category');
-    categories.data.forEach(category => {
-      let newOption = new Option(category.attributes.name, category.id)
-      dropDown.add(newOption, undefined)
+    const dropDown = document.getElementById('category'); // gets the select element
+    categories.data.forEach(category => { // for each category in the data object
+      let newOption = new Option(category.attributes.name, category.id) // create a new option with name key and id value
+      dropDown.add(newOption, undefined) // add the new option to the bottom of the dropdown list
     })
   })
+}
+
+document.getElementById('category-form-submit').addEventListener('click', getApples) 
+
+function getApples() {
+  let categoryId = parseInt(document.getElementById('category').value); 
+  fetch(`http://localhost:3000/api/v1/apples?category_id=${categoryId}`)
+  .then(response => response.json())
+  .then(apples => {
+    console.log(apples);
+  })
+  // add catch for error handling...if the array is empty, ask user to select a different option
 }
