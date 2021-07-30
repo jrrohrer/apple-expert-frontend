0b4f1e2d70e0f5dabc771c48186c9051dee9b5a6
jrrohrer <jessie.rohrer@gmail.com>
2021-07-30 12:30:18 -0400

    add dropdown category select

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("connected!")
+
+populateCategoryDropdown();
+
+function getCategories() {
+  return fetch("http://localhost:3000/api/v1/categories")
+    .then(response => response.json());
+    // add catch for error handling
+}
+
+function populateCategoryDropdown() {
+  getCategories()
+  .then(categories => {
+    const dropDown = document.getElementById('category');
+    categories.data.forEach(category => {
+      let newOption = new Option(category.attributes.name, category.id)
+      dropDown.add(newOption, undefined)
+    })
+  })
+}
