03b87dc3e0b2bdbbeba8e6198175f972ae7a2a59
jrrohrer <jessie.rohrer@gmail.com>
2021-08-02 16:06:27 -0400

    set up for OOJS refactor

--- src/index.js
+++ src/index.js
@@ -17,19 +17,21 @@ function populateCategoryDropdown() {
       dropDown.add(newOption, undefined) // add the new option to the bottom of the dropdown list
     })
   })
+  .catch(err => alert(err));
 }
 
 document.getElementById('category-form-submit').addEventListener('click', getApples) 
 
 function getApples() {
-  let categoryId = parseInt(document.getElementById('category').value); 
-  fetch(`http://localhost:3000/api/v1/apples?category_id=${categoryId}`)
+  let categoryId = parseInt(document.getElementById('category').value); // getting the category ID and turning it into an integer
+  fetch(`http://localhost:3000/api/v1/apples?category_id=${categoryId}`) // passing the category id to the apple controller as a query param so that only the apples that match taht category can be returned
   .then(response => response.json())
   .then(apples => {
     apples.data.forEach(apple => {
       displayApple(apple)
     })
   })
+  .catch(err => alert(err));
   // add catch for error handling...if the array is empty, ask user to select a different option
 }
 
@@ -45,3 +47,7 @@ function displayApple(apple) {
 
   document.querySelector('#apple-cards').innerHTML += appleCard;
 }
+
+// Next step: adding a form for creating a new apple & setting up the POST fetch
+// Then: adding an edit feature, adding a delete feature, error handling, and refactoring
+// Finally: styling!
