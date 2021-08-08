2b13348e4eb428016463a0c60f7b13b3d46e4f50
jrrohrer <jessie.rohrer@gmail.com>
2021-08-08 19:37:14 -0400

    building out apple constructor

--- src/apple.js
+++ src/apple.js
@@ -1,10 +1,21 @@
 class Apple {
-
+  constructor(appleId, appleAttributes) {
+    // setting properties of each apple
+    this.id = appleId
+    this.variety = appleAttributes.variety
+    this.harvest = appleAttributes.harvest
+    this.notes = appleAttributes.notes
+    this.image_url = appleAttributes.image_url
+    this.categories = appleAttributes.categories
+    Apple.all.push(this)
+  }
 }  
+
+Apple.all = []
   
 // build out apple class
 // remembering apples
-// setting properties of each apple
+
 // html we want on the DOM for each apple
 // attach event listeners particular to each item
 
