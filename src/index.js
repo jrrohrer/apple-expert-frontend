c89758d67026e527f1470e99d891e6d06ddb3728
jrrohrer <jessie.rohrer@gmail.com>
2021-08-18 14:09:38 -0400

    adding event listener to show/hide create apple form

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ const categoryApi = new CategoryApi(port)
 const categorySelectSubmit = document.getElementById('category-form-submit')
 const appleCardsContainer = document.getElementById('apple-cards')
 const createAppleForm = document.getElementById('create-apple-form')
+const makeNewAppleBtn = document.getElementById('make-new-apple')
 
 // Populate the dropdown menus with category objects so users can select/assign them dynamically:
 categoryApi.populateCategoryDropdown();
 
-
 // When user clicks "Show me the apples!", get the apples in that category, and display them in cards on the DOM:
 categorySelectSubmit.addEventListener('click', appleApi.getApples) 
 
+// event listener on this button shows/hides the create apple form
+makeNewAppleBtn.addEventListener('click', () => {
+  let div = document.getElementById('new-apple-form-container');
+  div.style.display = div.style.display == "none" ? "block" : "none";
+})
+
+
 // When the user clicks "Save New Apple", save the user's inputs to variables and use them to populate a post fetch to save the new apple object to the database.
 createAppleForm.addEventListener('submit', (e) => createFormHandler(e));
+
 
 function createFormHandler(e) {
   e.preventDefault();
