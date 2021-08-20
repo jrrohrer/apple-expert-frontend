3c29fe08e3a9d767fd445c530a2cf4ba6b991b69
jrrohrer <jessie.rohrer@gmail.com>
2021-08-20 17:15:36 -0400

    cleaning up old comments

--- src/appleApi.js
+++ src/appleApi.js
@@ -6,7 +6,7 @@ class AppleApi {
 
   getApples() {
     let categoryId = parseInt(document.getElementById('category').value); // getting the category ID and turning it into an integer
-    fetch(`${port}/apples?category_id=${categoryId}`) // passing the category id to the apple controller as a query param so that only the apples that match taht category can be returned
+    fetch(`${port}/apples?category_id=${categoryId}`)
     .then(response => response.json())
     .then(apples => {
       appleCardsContainer.innerHTML = ""; // clears any old search results before displaying new ones
@@ -28,8 +28,7 @@ class AppleApi {
     })
     .then(response => response.json())
     .then(apple => {
-      // Some UI work here: To let the user know their apple is successfully created, clear the apple-cards div of search results, add a success message, and display the new apple card
-      appleCardsContainer.innerHTML = "";
+      appleCardsContainer.innerHTML = ""; // clears previous results
       messageDiv.innerHTML = "<h3>Your apple has been saved to the database and will be included in future searches. Thanks for contributing to Apple Expert!</h3>"
       let a = new Apple(apple.data.id, apple.data.attributes)
       a.displayApple()
@@ -46,4 +45,3 @@ class AppleApi {
       .then(json => alert(json.message)) 
   }
 }
-
